a0e34de9db7b77558d0710ce7f79c9f5dd07d993
Tiago Magalhaes <up201607931@fe.up.pt>
2023-01-16 14:05:41 +0000

    added access operators to string view

--- Common/DataStructures/StringView.h
+++ Common/DataStructures/StringView.h
@@ -3,17 +3,28 @@
 #include "ArrayView.h"
 #include "DataStructures/ArrayView.h"
 #include "String.h"
+#include "Types.h"
 
 namespace Kolab
 {
 
-class StringView: public ArrayView<string>
+class StringView: public ArrayView<char>
 {
 public:
     StringView(const string& string)
     {
         this->array = string.ptr();
         this->length = string.length();
+    }
+
+    const char& operator[](uArchInt index)
+    {
+        return this->array[index];
+    }
+
+    char at(uArchInt index)
+    {
+        return this->array[index];
     }
 };
 
