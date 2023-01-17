c3984a39b810e19b795529d3be18fa5bb1020a85
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2023-01-17 23:11:44 +0000

    added script that handles installing system dependencies on posix systems

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,4 +3,5 @@
 #TODO(Tiago): need to add check for the type of windowing system and install the corresponding dependencies
 
 #for x11 systems
+sudo apt-get install -y llvm cmake ninja-build
 sudo apt install xorg-dev
