7517e8a4a047410f7fa0c5e4b50d92c4ac6a5675
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2023-01-17 23:06:46 +0000

    corrects ninja build system dependency installation

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,4 +2,4 @@
 
 mkdir build-ninja
 cd build-ninja
-cmake -G Ninja -DCMAKE_C_COMPILER="Clang" -DCMAKE_CXX_COMPILER="Clang" ..
+cmake -G Ninja -DCMAKE_C_COMPILER="clang" -DCMAKE_CXX_COMPILER="clang" ..
