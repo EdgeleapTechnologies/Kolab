4baf88b4d9d0c69019c7f9b322d80aa8bdacbeac
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2023-01-17 23:18:06 +0000

    added script that handles installing system dependencies on posix systems

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -6,3 +6,9 @@
 sudo apt-get update
 sudo apt-get install -y llvm cmake ninja-build --fix-missing
 sudo apt-get install -y xorg-dev --fix-missing
+sudo apt-get install -y mesa-utils libglu1-mesa-dev freeglut3-dev mesa-common-dev --fix-missing
+
+wget -qO- https://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo tee /etc/apt/trusted.gpg.d/lunarg.asc
+sudo wget -qO /etc/apt/sources.list.d/lunarg-vulkan-jammy.list http://packages.lunarg.com/vulkan/lunarg-vulkan-jammy.list
+sudo apt update
+sudo apt install vulkan-sdk
