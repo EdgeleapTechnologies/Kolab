c9e41605f2132356fb6bba19b7a4d89caec405de
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2023-01-18 01:39:53 +0000

    working on scripts for linting

---
+++ lint.ps1
@@ -0,0 +1,2 @@
+python ./run-clang-format.py --style file -r Common Client Server --exclude ThirdParty
+echo $?
--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -6,8 +6,11 @@
 sudo apt-get update
 sudo apt-get install -y llvm cmake ninja-build --fix-missing
 sudo apt-get install -y xorg-dev --fix-missing
+
+#install opengl dev libs
 sudo apt-get install -y mesa-utils libglu1-mesa-dev freeglut3-dev mesa-common-dev --fix-missing
 
+#install Vulkan dev libs from lunarg
 wget -qO- https://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo tee /etc/apt/trusted.gpg.d/lunarg.asc
 sudo wget -qO /etc/apt/sources.list.d/lunarg-vulkan-jammy.list http://packages.lunarg.com/vulkan/lunarg-vulkan-jammy.list
 sudo apt update
