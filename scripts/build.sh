63ca3a7dd690e51975252fa8707ac7feea6f4458
Tiago Magalhaes <up201607931@fe.up.pt>
2023-02-07 03:13:39 +0000

    added ability to swap between compilers on linux builds

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+CXX_COMPILER="clang++"
+C_COMPILER="clang"
+if [ $# -eq 1 ]; then
+    if [ "$1" = "--clang" ]; then
+        C_COMPILER="clang"
+        CXX_COMPILER="clang++"
+    fi
+    if [ "$1" = "--gcc" ]; then
+        C_COMPILER="gcc"
+        CXX_COMPILER="g++"
+    fi
+fi
+
 mkdir build-ninja
 cd build-ninja
-cmake -G Ninja -DCMAKE_C_COMPILER="clang" -DCMAKE_CXX_COMPILER="clang++" ..
+cmake -G Ninja -DCMAKE_C_COMPILER=$C_COMPILER -DCMAKE_CXX_COMPILER=$CXX_COMPILER ..
 cmake --build .
