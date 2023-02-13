c2851ed378598d55809631a65ee5abd19f57a4b9
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2023-02-13 01:48:54 +0000

    working on build script and output layout

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -3,16 +3,39 @@ cd ..
 
 $compiler_name="clang"
 $compiler="Clang"
+$generator="Ninja"
+if($args.count -gt 0)
+{
+    for ( $i = 0; $i -lt $args.count; $i++ ) {
+        switch($args[$i])
+        {
+            "--clang" {
+                $generator="Ninja"
+                $compiler_name="clang"
+                $compiler="Clang"
+            }
+            "--msvc" {
+                $generator="Visual Studio 17 2022"
+                $compiler_name="msvc"
+                $compiler="cl.exe"
+            }
+        }
+    } 
+}
+
+if (-not (Test-Path -LiteralPath ./build/))
+{
+    mkdir build > $null
+}
 if (-not (Test-Path -LiteralPath ./build/windows-$compiler_name)) {
-    mkdir build
-    mkdir build/windows-$compiler_name
+    mkdir build/windows-$compiler_name > $null
     cd build/windows-$compiler_name
-    cmake -G Ninja -DCMAKE_C_COMPILER="$compiler" -DCMAKE_CXX_COMPILER="$compiler" ../..
+    cmake -G $generator -DCMAKE_C_COMPILER="$compiler" -DCMAKE_CXX_COMPILER="$compiler" ../..
     cd ../..
 }
 
 cd ./build/windows-$compiler_name
-cmake --build .
+cmake --build . -j
 $outcode = $LASTEXITCODE
 
 if (Test-Path -LiteralPath compile_commands.json) {
