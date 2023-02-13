cad0fcbc06ee02bc421fff798e0fcd10d42e8719
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2023-02-13 23:23:20 +0000

    started refactoring linux build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,8 @@
 # clang is used as the default compiler
 CXX_COMPILER="clang++"
 C_COMPILER="clang"
+BUILD_TESTS=$false
+BUILD_ONLY_TEST=$false
 for var in "$@"
 do
     if [ "$var" = "--clang" ]; then
@@ -13,11 +15,31 @@ do
         C_COMPILER="gcc"
         CXX_COMPILER="g++"
     fi
+    if [ "$var" = "--test" ]; then
+        BUILD_TESTS=$true
+    fi
+    if [ "$var" = "--test-only" ]; then
+        BUILD_TESTS=$true
+        BUILD_ONLY_TEST=$true
+    fi
 done
 
-mkdir build
-mkdir build/linux-$C_COMPILER
-cd build/linux-$C_COMPILER
 
-cmake -G Ninja -DCMAKE_C_COMPILER=$C_COMPILER -DCMAKE_CXX_COMPILER=$CXX_COMPILER ../..
-cmake --build .
+if [ ! -d build/linux-$C_COMPILER ]; then
+    mkdir build
+    mkdir build/linux-$C_COMPILER
+    cd build/linux-$C_COMPILER
+    cmake -G Ninja -DCMAKE_C_COMPILER=$C_COMPILER -DCMAKE_CXX_COMPILER=$CXX_COMPILER ../..
+fi
+
+if [ $BUILD_ONLY_TEST -eq $false ]; then
+    cmake --build . -j
+fi
+if [ $BUILD_TESTS -eq $false ]; then
+    cmake --build . -j --target kolab-tests
+fi
+
+if [ -e compile_commands.json ]; then
+    cp compile_commands.json ../..
+fi
+cd ../..
