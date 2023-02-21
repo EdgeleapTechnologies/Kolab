34b56178bdcbc9b812f990c996c4cccf1277ccab
Tiago Magalhaes <up201607931@fe.up.pt>
2023-02-21 01:45:26 +0000

    added build type support to posix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,43 +3,58 @@
 # clang is used as the default compiler
 CXX_COMPILER="clang++"
 C_COMPILER="clang"
-BUILD_TESTS=$false
-BUILD_ONLY_TEST=$false
+BUILD_TESTS=false
+BUILD_ONLY_TEST=false
+BUILD_TYPE="Debug"
+BUILD_TYPE_NAME="debug"
 for var in "$@"
 do
-    if [ "$var" = "--clang" ]; then
-        C_COMPILER="clang"
-        CXX_COMPILER="clang++"
-    fi
-    if [ "$var" = "--gcc" ]; then
-        C_COMPILER="gcc"
-        CXX_COMPILER="g++"
-    fi
-    if [ "$var" = "--test" ]; then
-        BUILD_TESTS=$true
-    fi
-    if [ "$var" = "--test-only" ]; then
-        BUILD_TESTS=$true
-        BUILD_ONLY_TEST=$true
-    fi
+    case $var in
+        "--clang")
+            C_COMPILER="clang"
+            CXX_COMPILER="clang++"
+            ;;
+        "--gcc")
+            C_COMPILER="gcc"
+            CXX_COMPILER="g++"
+            ;;
+        "--test")
+            BUILD_TESTS=true
+            ;;
+        "--test-only")
+            BUILD_TESTS=true
+            BUILD_ONLY_TEST=true
+            ;;
+        "--debug")
+            BUILD_TYPE="Debug"
+            BUILD_TYPE_NAME="debug"
+            ;;
+        "--release")
+            BUILD_TYPE="Release"
+            BUILD_TYPE_NAME="release"
+            ;;
+        *);;
+    esac
 done
 
 
-if [ ! -d build/linux-$C_COMPILER ]; then
+if [ ! -d build/linux-$C_COMPILER-$BUILD_TYPE_NAME ]; then
     mkdir build
-    mkdir build/linux-$C_COMPILER
-    cd build/linux-$C_COMPILER
-    cmake -G Ninja -DCMAKE_C_COMPILER=$C_COMPILER -DCMAKE_CXX_COMPILER=$CXX_COMPILER ../..
+    mkdir build/linux-$C_COMPILER-$BUILD_TYPE_NAME
+    cd build/linux-$C_COMPILER-$BUILD_TYPE_NAME
+    cmake -G Ninja -DCMAKE_C_COMPILER=$C_COMPILER -DCMAKE_CXX_COMPILER=$CXX_COMPILER -DCMAKE_BUILD_TYPE=$BUILD_TYPE ../..
 fi
 
-if [ $BUILD_ONLY_TEST -eq $false ]; then
+cd build/linux-$C_COMPILER-$BUILD_TYPE_NAME/
+if [ $BUILD_ONLY_TEST = false ]; then
     cmake --build . -j
 fi
-if [ $BUILD_TESTS -eq $true ]; then
+
+if [ $BUILD_TESTS = true ]; then
     cmake --build . -j --target kolab-tests
 fi
 
 if [ -e compile_commands.json ]; then
     cp compile_commands.json ../..
 fi
-cd ../..
+
